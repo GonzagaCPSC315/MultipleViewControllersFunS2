055963c6e1eb65f9999b5f2d63bbed207bbabb6d
Gina Sprint <sprint@gonzaga.edu>
2020-10-16 11:36:43 -0700

    start of prepare

--- MultipleViewControllersFunS2/InitialViewController.swift
+++ MultipleViewControllersFunS2/InitialViewController.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
+// MARK: - Multiple View Controllers
+// so far, all of our apps have had one screen
+// recall: 1 scene in IB = 1 screen = managed by 1 view controller
+
+// MARK: - Segues
+// a segue is a transition from one view controller to another
+// 2 types
+// 1. automatic (let IB handle)
+// 2. manual (we will trigger the segue in Swift)
+
+
+
 class InitialViewController: UIViewController {
     @IBOutlet var usernameTextField: UITextField!
 
@@ -18,6 +30,19 @@ class InitialViewController: UIViewController {
 
     @IBAction func unwindToInitialVC(segue: UIStoryboardSegue) {
         print("unwinding to initial view controller")
+    }
+    
+    // we want to grab the text from the username text field and send it into our SecondViewController
+    // prepare(for:sender:) is called right before a segue occurs
+    // override it!!
+    // here that we will pass data into the second view controller
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // sender: is the view that triggered the segue (if there is one)
+        // segue has 3 important properties
+        // source: this is the instigating view controller
+        // destination: this is the new view controller
+        // identifier: this is the string you may have set up in IB
+        
     }
 
 }
